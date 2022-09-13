c9cdb323f2bac0460e8f45bd0dd2062aa874bfd1
LiJoBaZar <lijobazar@gmail.com>
2022-09-13 08:38:52 -0700

    correction output

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 /**
- *bubble_sort - used to sort a given set of n elements.
- *provided in form of an array with n number of elements.
- *Bubble Sort compares all the element one by one and sort.
+ *bubble_sort - used to sort a given set of n elements,
+ *compares all the element one by one and sort
  *them based on their values.
+ *
  *@array: array of data to be sorted
  *@size: size of the array
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, j = 0, sorted = 1, tmp = 0;
+	size_t i = 0;
+	int sorted = 1, tmp = 0;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
-	j = size;
-	while (sorted == 1 && j > 0)
+
+	while (size)
 	{
 		sorted = 0;
-		i = 1;
-		while (i <= j)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i - 1] > array[i] && array[i] != '\0')
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[i - 1];
-				array[i - 1] = array[i];
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
 				array[i] = tmp;
 				print_array(array, size);
 				sorted = 1;
 			}
-			i += 1;
 		}
-		j -= 1;
+		if (!sorted)
+			break;
 	}
 }
